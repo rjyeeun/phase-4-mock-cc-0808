42742cacf5b24ca7ee90bec09d6bf92727581aaa
Yeeun Jung <rjyeeun@gmail.com>
2023-03-01 16:43:22 -0500

    commit tree 08a3d1756f0d0cb6788e93fc398dd5923e739b1d

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,18 +1,18 @@
 class SignupsController < ApplicationController
     rescue_from ActiveRecord::RecordInvalid, with: :signup_not_valid
-
+    
     def create
-        signup = Signup.create(signup_params)
-        render json: signup, status: 201
+        signup = Signup.create!(signup_params)
+        render json: signup.activity, status: 201
     end
 
     private 
 
-    def signup_not_valid 
-        render json: {error: "validation errors"}, status: :unprocessable_entity
+    def signup_params
+        params.permit(:camper_id, :activity_id, :time)
     end
 
-    def signup_params
-        params.permit(:camper_id, :activity_id, :time, :activities)
+    def signup_not_valid 
+        render json: {"errors": ["validation errors"]}, status: 422
     end
 end
--- db/migrate/20230301153154_create_signups.rb
+++ db/migrate/20230301153154_create_signups.rb
@@ -1,8 +1,8 @@
 class CreateSignups < ActiveRecord::Migration[6.1]
   def change
     create_table :signups do |t|
-      t.belongs_to :camper, null: false, foreign_key: true
-      t.belongs_to :activity, null: false, foreign_key: true
+      t.integer :camper_id
+      t.integer :activity_id
       t.integer :time
 
       t.timestamps
